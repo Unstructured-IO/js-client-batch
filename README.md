e756148eb5132a4a967cd69e7b0a91fb7423a5bf
Treighton Mauldin <treightonmauldin@TreightonsMacBook.local>
2024-07-27 10:53:30 -0700

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# Batch Processing Documents with Unstructured JS SDK
+
+This script will process all documents in a directory using the Unstructured Serverless API JavaScript SDK.
+
+## Setup
+
+1. Clone the repo `git clone git@github.com:Unstructured-IO/js-client-batch.git`
+2. Install dependencies `npm i`
+3. Set environment variables in `.env`
+
+```
+UNSTRUCTURED_API_URL=https://api.unstructuredapp.io || <the URL of your choice>
+UNSTRUCTURED_API_KEY=<Your Unstructured API Key>
+STRATEGY='fast' || 'hi_res'
+SPLIT_PAGES='true' || 'false'
+```
+
+## Running
+
+### Documents to process
+
+By default this script looks to `./sample_data` for files to process, or you can specify the path as in `.env` as:
+
+```
+...
+DOCS_PATH=full/path/to/documents
+```
+
+### Output
+
+By default this script will output partitioned results to `./output` or you can specify the output path in `.env` as:
+
+```
+...
+OUTPUT_PATH=full/path/to/output
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+import { UnstructuredClient } from "unstructured-client";
+import { readdir, readFile, writeFile } from "node:fs/promises";
+import { createLogger, format, transports } from "winston";
+import { fileURLToPath } from 'url';
+import path from 'path';
+import 'dotenv/config'
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
+
+const url = process.env.UNSTRUCTURED_API_URL
+const key = process.env.UNSTRUCTURED_API_KEY
+const strategy = process.env.STRATEGY || 'fast'
+const splitPages = process.env.SPLIT_PAGES === 'true' ? true : false
+const docsPath = process.env.DOCS_PATH || path.resolve(__dirname, "./sample_data");
+const outputPath = process.env.OUTPUT_PATH || path.resolve(__dirname, "./output");
+
+const logger = createLogger({
+  level: 'info',
+  format: format.json(),
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+})
+
+const client = new UnstructuredClient({
+  serverURL: url,
+  security: {
+    apiKeyAuth: key,
+  },
+});
+
+const getFilesWithPath = async (docsPath) => {
+    const files = await readdir(docsPath);
+    return files.map(file => `${docsPath}/${file}`)
+}
+
+const getFileData = async (filePath) => {
+    try {
+        const fileBuffer = await readFile(filePath)
+        return fileBuffer
+    } catch(e) {
+        throw new Error(`Failed to read ${filePath}`)
+    }
+}
+
+const partition = async (data, file) => await client.general.partition({
+    partitionParameters: {
+      files: {
+        content: data,
+        fileName: file,
+      },
+      strategy: strategy,
+      splitPdfPage: splitPages
+    },
+  });
+
+const writeResults = async (outputPath, file, res) => {
+    const outputName = file.split('/')[1]
+    await writeFile(
+        `${outputPath}/${outputName}.json`,
+        JSON.stringify(res.elements),
+        (err) => console.info(err),
+      );
+    return true
+}
+
+async function main() {
+    logger.profile(`Overall Runtime with strategy ${strategy} and split pages ${splitPages}`)
+
+    const files = await getFilesWithPath(docsPath)
+    
+    for (const file of files) {
+      try {
+        logger.profile(`Partitioning ${file}`)
+        const fileBuffer = await getFileData(file)
+        const partitionedFile = await partition(fileBuffer, file)
+        const writePartitionResults = await writeResults(outputPath, file, partitionedFile)
+    
+        if (writePartitionResults) {
+          logger.profile(`Partitioning ${file}`)
+          logger.info(`Partitioning ${file} complete`)
+        }
+        
+      } catch (e) {
+        logger.error(`Failed to process ${file}`, e)
+      }
+    }
+    logger.profile(`Overall Runtime with strategy ${strategy} and split pages ${splitPages}`)
+}
+
+main()
